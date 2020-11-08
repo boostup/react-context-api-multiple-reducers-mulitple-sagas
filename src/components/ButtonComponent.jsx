b2102a440a52648e33b3cc53431f77d5b5462832
boostup <boostup@gmail.com>
2020-11-08 12:08:54 +0100

    pretty print responds to state updates

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useDataLayerValue } from "../context/DataLayer";
 
-const ButtonComponent = ({ instructions, onClick }) => (
-  <button onClick={onClick}>{instructions} (Takes from 1 to 3 seconds)</button>
-);
+const ButtonComponent = ({ instructions, onClick }) => {
+  const { dispatch } = useDataLayerValue();
+
+  function handleClick() {
+    dispatch({ type: "IS_WORKING", payload: true });
+    onClick();
+
+    let timer = setTimeout(() => {
+      dispatch({ type: "IS_WORKING", payload: false });
+    }, 1000);
+  }
+
+  return <button onClick={handleClick}>{instructions}</button>;
+};
 export default ButtonComponent;
--- src/components/PrettyPrint.jsx
+++ src/components/PrettyPrint.jsx
@@ -1,5 +1,27 @@
-import React from "react";
-const PrettyPrint = ({ toPrint }) => (
-  <pre>{JSON.stringify(toPrint, null, 2)}</pre>
-);
+import React, { useEffect, useLayoutEffect, useState } from "react";
+const PrettyPrint = ({ toPrint }) => {
+  const [In, setIn] = useState("---");
+
+  useEffect(() => {
+    let timer = null;
+
+    setIn("Out");
+
+    timer = setTimeout(() => {
+      setIn("In");
+    }, 100);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [toPrint.counter1, toPrint.counter2]);
+
+  return (
+    <div className="prettyPrint">
+      <pre title="I animate when counters increment!" className={In}>
+        {JSON.stringify(toPrint, null, 2)}
+      </pre>
+    </div>
+  );
+};
 export default PrettyPrint;
