67ac87faaa11f032271efbf23a916c3ac40c08db
boostup <boostup@gmail.com>
2020-11-09 09:08:25 +0100

    some refactor

--- src/components/PrettyPrint.jsx
+++ src/components/PrettyPrint.jsx
@@ -3,21 +3,11 @@ import React, { useEffect, useState } from "react";
 import DataTable from "./DataTable";
 
 const PrettyPrint = ({ toPrint }) => {
-  const [In, setIn] = useState("Out");
+  const [In, setIn] = useState("In");
 
   useEffect(() => {
-    let timer = null;
-
-    setIn("Out");
-
-    timer = setTimeout(() => {
-      setIn("In");
-    }, 100);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [toPrint.counter1, toPrint.counter2, toPrint.chuckNorris]);
+    setIn(In === "In" ? "Out" : "In");
+  }, [toPrint.ui.isWorking]);
 
   return (
     <div className="prettyPrint">
